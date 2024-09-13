3e8b289a80c24c4fbda1f4a2f44f9572b925143e
Shahzaib Ali <smsashafqat@gmail.com>
2024-09-14 00:22:05 +0500

    code updated

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI, Form, status, Response
-import time, json, requests
+from fastapi import FastAPI, status, Response
+from pydantic import BaseModel
+import time, json, requests, base64
 
 app = FastAPI()
+
+
+class DataModel(BaseModel):
+    phone: str
+    body: str
 
 
 def read_json_file(file_path):
@@ -12,12 +18,7 @@ def read_json_file(file_path):
         return []
 
 
-def write_json_file(file_path, data):
-    with open(file_path, "w") as file:
-        json.dump(data, file, indent=4)
-
-
-def get_access_token():
+def get_access_token() -> str:
     token_file = "tokens.json"
 
     data = read_json_file(token_file)
@@ -27,7 +28,9 @@ def get_access_token():
     token_time = data["created_time"]
 
     if time.time() - token_time > expires_in:
-        client_secret = "f96a3527059cc346B84b5b5d960cb21006b41d11587aa4edDa2Cb94142b4c2c7ea0182cc"
+        client_secret = (
+            "f96a3527059cc346B84b5b5d960cb21006b41d11587aa4edDa2Cb94142b4c2c7ea0182cc"
+        )
         client_id = "c3FcCa19c94547ed9f6c4Ed9eF52180834bf1637"
 
         url = "https://api01.sms.fpt.net/oauth2/token"
@@ -38,46 +41,60 @@ def get_access_token():
             "grant_type": "client_credentials",
             "client_id": client_id,
             "client_secret": client_secret,
-            "scope": "send_brandname, send_brandname_otp",
-            "session_id": "5c22be0c0396440829c98d7ba1240920"
+            "scope": "send_brandname_otp",
+            "session_id": "5c22be0c0396440829c98d7ba1240920",
         }
 
-        tokens = requests.post(url, data=json.dumps(data), headers=headers, verify=False).json()
-        print(tokens)
-        access_token = tokens["access_token"]
-        tokens["created_time"] = time.time()
+        try:
+            response = requests.post(url, json=data, headers=headers)
+            response.raise_for_status()  # Raise an error for bad HTTP responses
+            tokens = response.json()
+        except requests.RequestException as e:
+            print(f"Error getting access token: {e}")
+            return f"Failed to get access token: {e}"
 
-        write_json_file(token_file, tokens)
+        access_token = tokens.get("access_token", "")
+        tokens["created_time"] = time.time()
+        with open(token_file, "w") as file:
+            json.dump(tokens, file, indent=4)
+
     return access_token
 
 
 @app.post("/send/")
-async def handle_form(
-    phone: str = Form(...), body: str = Form(...), sender: str = Form(...)
-):
+async def handle_form(data: DataModel):
     try:
-        url = "http://app.sms.fpt.net/api/push-brandname-international"
+        bytes_string = data.body.encode("utf-8")
+        base64_encoded = base64.b64encode(bytes_string).decode("utf-8")
+        url = "http://api01.sms.fpt.net/api/push-brandname-otp"
         access_token = get_access_token()
+
+        if (
+            access_token == "No token data available"
+            or "Failed to get access token" in access_token
+        ):
+            return Response(
+                content=json.dumps({"error": access_token}),
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                media_type="application/json",
+            )
+
         headers = {
             "Content-Type": "application/json",
         }
         data = {
             "access_token": access_token,
             "session_id": "5c22be0c0396440829c98d7ba124092020145753419",
-            "BrandName": "FTI",
-            "Phone": phone,
-            "Message": body,
+            "BrandName": "ARROWSTER",
+            "Phone": data.phone,
+            "Message": base64_encoded,
         }
-        return {"msg": "Message sent successfully"}
-    except:
+        resp = requests.post(url, json=data, headers=headers)
+        resp.raise_for_status()
+        return resp.json()
+    except Exception as e:
+        print(f"Error handling form: {e}")
         return Response(
-            content={"error": "Unable to send msg."},
+            content={"error": str(e)},
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
-
-
-@app.post("/validate/")
-async def validate(generated_otp: str, user_otp: str):
-    if generated_otp == user_otp:
-        return True
-    return False
